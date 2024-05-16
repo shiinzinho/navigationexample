8373b084332ac1b85d58b0705337f21eaa480eab
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-05-16 11:02:41 -0300

    update

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "./src/screens/Home";
 import Profile from "./src/screens/Profile";
 import PesquisaProduto from "./src/screens/PesquisaProduto";
+import EditarProduto from "./src/screens/EditarProduto";
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ function App(): React.JSX.Element{
       <Stack.Navigator>
         <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
         <Stack.Screen name='Profile' component={Profile} options={{headerShown: false}}/>
+        <Stack.Screen name='EditarProduto' component={EditarProduto} options={{headerShown: false}}/>
         <Stack.Screen name='PesquisaProduto' component={PesquisaProduto} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
